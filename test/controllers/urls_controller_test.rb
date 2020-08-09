21247d90f8e911debab3e2268ab202462057192d
Clint Lauby <thecmachine05@gmail.com>
2020-08-09 10:37:55 -0400

    added user tests

--- test/controllers/urls_controller_test.rb
+++ test/controllers/urls_controller_test.rb
@@ -4,12 +4,12 @@ class UrlsControllerTest < ActionDispatch::IntegrationTest
   test "should not save a url without a link" do
     url = Url.new
     url.short = "localhost:3000//asdfasd"
-    assert_not url.save, "Saved the project without a link"
+    assert_not url.save, "Saved the url without a link"
   end
 
   test "should not save a url without a short" do
     url = Url.new
     url.link = "www.google.com"
-    assert_not url.save, "Saved the project without a link"
+    assert_not url.save, "Saved the url without a short"
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save a user without a password" do
+    user = User.new
+    user.name = 'name'
+    user.email = 'name@test.com'
+    assert_not user.save, "Saved the user without a password"
+  end
+  test "should not save a user without a name" do
+    user = User.new
+    user.password = 'password'
+    user.email = 'name@test.com'
+    assert_not user.save, "Saved the user without a name"
+  end
+  test "should not save a user without an email" do
+    user = User.new
+    user.password = 'password'
+    user.name = 'name'
+    assert_not user.save, "Saved the user without an email"
+  end
 end
