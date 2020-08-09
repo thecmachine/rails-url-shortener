50106726f9a376c377b687b8113fec758853e37d
Clint Lauby <thecmachine05@gmail.com>
2020-08-09 10:02:27 -0400

    added some tests

--- test/controllers/urls_controller_test.rb
+++ test/controllers/urls_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UrlsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save a url without a link" do
+    url = Url.new
+    url.short = "localhost:3000//asdfasd"
+    assert_not url.save, "Saved the project without a link"
+  end
+
+  test "should not save a url without a short" do
+    url = Url.new
+    url.link = "www.google.com"
+    assert_not url.save, "Saved the project without a link"
+  end
 end
