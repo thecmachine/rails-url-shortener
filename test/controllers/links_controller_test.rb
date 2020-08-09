fed5b8a4a70544da1795ed48aa90735a2bcef613
Clint Lauby <thecmachine05@gmail.com>
2020-08-09 11:42:44 -0400

    link test works

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class LinksControllerTest < ActionDispatch::IntegrationTest
-  test "the truth" do
-    assert true
+  test "should redirect user to link" do
+    user = User.new
+    user.name = 'name'
+    user.email = 'name@test.com'
+    user.password = 'password'
+    user.save
+
+    url = Url.new
+    url.user = user
+    url.link = "www.google.com"
+    url.short = "localhost:3000/links/" + (0...8).map { (65 + rand(26)).chr }.join
+    url.save
+
+    urllink = Url.find_by_sql("Select link from urls where short = '" + url.short + "'")
+    urllink = urllink[0].link
+    link = "http://" + urllink
+    assert_equal("http://www.google.com", link)
+  end
+  test "should redirect user to link are not equal" do
+    user = User.new
+    user.name = 'name'
+    user.email = 'name@test.com'
+    user.password = 'password'
+    user.save
+
+    url = Url.new
+    url.user = user
+    url.link = "www.test.com"
+    url.short = "localhost:3000/links/" + (0...8).map { (65 + rand(26)).chr }.join
+    url.save
+
+    urllink = Url.find_by_sql("Select link from urls where short = '" + url.short + "'")
+    urllink = urllink[0].link
+    link = "http://" + urllink
+    assert_not_equal("http://www.google.com", link)
   end
 end
