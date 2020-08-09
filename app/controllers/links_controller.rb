b95c0d0f7f72d05cf1df4208e1f7a5b804dc7972
Clint Lauby <thecmachine05@gmail.com>
2020-08-09 09:32:21 -0400

    shortener works

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,8 +1,8 @@
 class LinksController < ApplicationController
   def index
-    # @redirect = params[:id]
     @redirect = Url.find_by_sql("Select link from urls where short = 'localhost:3000/links/" + params[:id] + "'")
-
-    redirect_to(@redirect)
+    @redirect = @redirect[0].link
+    @link = "http://" + @redirect
+    redirect_to @link
   end
 end
